d6928b21960ec2ddddbaa5e2cced6fdf99a565db
LeonW <leon_1607@hotmail.de>
2024-05-17 13:06:11 +0200

    implemented slash commands

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
-from typing import Final, List
+from typing import Final, List, Optional
 from dotenv import dotenv_values
-from discord import Intents, Client, Message, VoiceChannel, Guild, Member
+from discord import Intents, Client, Message, VoiceChannel, Guild, Member, app_commands
 import discord
 
 
@@ -19,6 +19,61 @@ intents.message_content = True  #NOQA
 intents.guilds = True  # Ensure guild intents are enabled to access guild information
 intents.members = True  # Enable intents to access member information
 client: Client = Client(intents=intents)
+
+### Slash Examples ###
+
+tree = app_commands.CommandTree(client)
+
+MY_GUILD = discord.Object(id=885183686646042724)
+
+
+@tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+
+@tree.command()
+@app_commands.describe(
+    first_value='The first value you want to add something to',
+    second_value='The value you want to add to the first value',
+)
+async def add(interaction: discord.Interaction, first_value: int, second_value: int):
+    """Adds two numbers together."""
+    await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
+    
+    
+# To make an argument optional, you can either give it a supported default argument
+# or you can mark it as Optional from the typing standard library. This example does both.
+@tree.command()
+@app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the command')
+async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
+    """Says when a member joined."""
+    # If no member is explicitly provided then we use the command user here
+    member = member or interaction.user
+
+    # The format_dt function formats the date time into a human readable representation in the official client
+    await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+
+### Slash Examples End ###
+
+
+@tree.command()
+async def teams(interaction: discord.Interaction):
+    """Creates and moves users to teams."""
+    # Check if the author is an administrator
+    if not interaction.user.guild_permissions.administrator:
+        await interaction.response.send_message("You do not have permission to use this command.")
+        return
+
+    # Check if the user is in the 'CS2' voice channel
+    if str(interaction.channel) != 'CS2':
+        await interaction.response.send_message("You need to be in the 'CS2' voice channel to use this command.")
+        return
+
+    statusMsg = await create_and_move_teams(interaction)
+    await interaction.response.send_message(statusMsg)
+    return
 
 
 # Get all voice channels in the guild
@@ -45,12 +100,16 @@ async def move_members(members: List[Member], target_channel: VoiceChannel):
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
+    
+    tree.copy_global_to(guild=MY_GUILD)
+    await tree.sync(guild=MY_GUILD)
+        
     for guild in client.guilds:
         voice_channels = await get_voice_channels(guild)
         print(f'Voice channels in guild {guild.name}:')
         for channel in voice_channels:
             print(f' - {channel.name}')
-    
+                
 
 @client.event
 async def on_message(message: Message) -> None:
@@ -63,17 +122,9 @@ async def on_message(message: Message) -> None:
 
     print(f'[{channel}] {username}: "{user_message}"')
 
-    # Check for the /teams command
-    if user_message.lower() == '/teams' and channel == 'CS2':
-        # Check if the author is an administrator
-        if message.author.guild_permissions.administrator:
-            print("Creating and moving users to teams")
-            await create_and_move_teams(message)
-        else:
-            await message.channel.send("You do not have permission to use this command.")
-
+    # ToDo: Convert to slash command
     # Check for the /back command
-    elif user_message.lower() == '/back':
+    if user_message.lower() == '/back':
         # Check if the author has permission to move members
         if message.author.guild_permissions.move_members:
             print("Moving users back to CS2")
@@ -110,11 +161,14 @@ async def create_and_move_teams(message: Message) -> None:
                 f"Terrorist: {', '.join(team1_names)} (moved to 'Terrorist' voice channel)\n"
                 f"Anti Terrorist: {', '.join(team2_names)} (moved to 'Anti Terrorist' voice channel)"
             )
-            await message.channel.send(response)
+            # await message.channel.send(response)
+            return response
         else:
-            await message.channel.send("Not enough members in the CS2 channel to create teams.")
+            # await message.channel.send("Not enough members in the CS2 channel to create teams.")
+            return "Not enough members in the CS2 channel to create teams."
     else:
-        await message.channel.send("Could not find the necessary voice channels ('CS2', 'Terrorist', 'Anti Terrorist').")
+        # await message.channel.send("Could not find the necessary voice channels ('CS2', 'Terrorist', 'Anti Terrorist').")
+        return "Could not find the necessary voice channels ('CS2', 'Terrorist', 'Anti Terrorist')."
 
 
 async def move_to_lobby(message: Message) -> None:
@@ -132,7 +186,6 @@ async def move_to_lobby(message: Message) -> None:
         await message.channel.send("Moved all members back to CS2.")
     else:
         await message.channel.send("Could not find the necessary channels or you do not have permission to move members.")
-
 
 
 def main() -> None:
